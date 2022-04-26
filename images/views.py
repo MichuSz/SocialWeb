cc84e290173753bb2e3999f3ce10ab151480bfda
MichuSz <szmania_michal@yahoo.com>
2022-04-26 11:48:44 +0200

    whats new activities

--- images/views.py
+++ images/views.py
@@ -7,6 +7,7 @@ from .models import Image
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from actions.utils import create_action
 
 
 @login_required
@@ -18,6 +19,7 @@ def image_create(request):
             new_item = form.save(commit=False)
             new_item.user = request.user
             new_item.save()
+            create_action(request.user,'dodał obraz',new_item)
             messages.success(request, 'Obraz został dodany.')
             return redirect(new_item.get_absolute_url())
     else:
@@ -41,6 +43,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user,'polubił', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status':'ok'})
